8d86b9d7b554c7b12e757547d75ff174c15ebe9c
Nicolas Pouillon <nipo@ssji.net>
2017-12-05 13:06:16 +0100

    Add Arcade/VR usage pages

---
+++ hrdc/usage/vr.py
@@ -0,0 +1,14 @@
+from usage import *
+
+Belt = Usage("vr.Belt", 0x30001, CA)
+BodySuit = Usage("vr.BodySuit", 0x30002, CA)
+Flexor = Usage("vr.Flexor", 0x30003, CP)
+Glove = Usage("vr.Glove", 0x30004, CA)
+HeadTracker = Usage("vr.HeadTracker", 0x30005, CP)
+HeadMountedDisplay = Usage("vr.HeadMountedDisplay", 0x30006, CA)
+HandTracker = Usage("vr.HandTracker", 0x30007, CA)
+Oculometer = Usage("vr.Oculometer", 0x30008, CA)
+Vest = Usage("vr.Vest", 0x30009, CA)
+AnimatronicDevice = Usage("vr.AnimatronicDevice", 0x3000A, CA)
+StereoEnable = Usage("vr.StereoEnable", 0x30020, OOC)
+DisplayEnable = Usage("vr.DisplayEnable", 0x30021, OOC)
